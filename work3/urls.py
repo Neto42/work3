1388604e9c72ab8f68f887f8001f303b81afec49
Neto42 <Neto42@users.noreply.github.com>
2021-01-15 18:26:33 +0300

    urls work and ads

--- work3/urls.py
+++ work3/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/works/', permanent=True)),
 ]
